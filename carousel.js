89eb1cee8fb31bb2dd820e54d31a6330e2ead1b3
Grixtynine <grixyirlaz7@gmail.com>
2026-08-30 21:59:06 -0500

    Small Changes

--- carousel.js
+++ carousel.js
@@ -1,5 +1,5 @@
 const imagesList = [
-    {"src":"images/1.jpg","link":"https://google.com","alt":"","name":"image 1"},
+    {"src":"images/Aspect-ratio-16x9.png","link":"https://google.com","alt":"","name":"image 1"},
     {"src":"images/2.jpg","link":"https://youtube.com","alt":"","name":"image 2"},
 
 ];
